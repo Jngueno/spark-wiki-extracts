7dc648947a833dfffa3fa8dfe5cc51fb5e1df344
Jennyfer NGUENO <ange-jennyfer.ngueno-fokam@francetv.fr>
2018-04-25 11:10:30 +0200

    I've tried but not compiling: Finished assigned tasks

--- src/main/scala/com/test/spark/wiki/extracts/Q1_WikiDocumentsToParquetTask.scala
+++ src/main/scala/com/test/spark/wiki/extracts/Q1_WikiDocumentsToParquetTask.scala
@@ -1,5 +1,6 @@
 package com.test.spark.wiki.extracts
 
+import java.io.FileReader
 import java.net.URL
 
 import com.fasterxml.jackson.annotation.JsonProperty
@@ -8,21 +9,24 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.joda.time.DateTime
 import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document, Element}
+import org.jsoup.select.Elements
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.JavaConversions._
 
 case class Q1_WikiDocumentsToParquetTask(bucket: String) extends Runnable {
   private val session: SparkSession = SparkSession.builder().getOrCreate()
+  import session.implicits._
   private val logger: Logger = LoggerFactory.getLogger(getClass)
 
   override def run(): Unit = {
     val toDate = DateTime.now().withYear(2017)
     val fromDate = toDate.minusYears(40)
 
+    // TODO Q1 Transformer cette seq en dataset
     getLeagues
-      // TODO Q1 Transformer cette seq en dataset
-      .XXXXX
+      .toDS()
       .flatMap {
         input =>
           (fromDate.getYear until toDate.getYear).map {
@@ -37,26 +41,59 @@ case class Q1_WikiDocumentsToParquetTask(bucket: String) extends Runnable {
             // ATTENTION:
             //  - Quelques pages ne respectent pas le format commun et pourraient planter - pas grave
             //  - Il faut veillez à recuperer uniquement le classement général
-            //  - Il faut normaliser les colonnes "positions", "teams" et "points" en cas de problèmes de formatage
+            //  - Il faut normaliser les colonnes "positions", "teams" et "points" en cas de problèmes de formatageimport org.jsoup.Jsoup
+
+            val leagueStandings = Seq(LeagueStanding)
+            val classementPage = Jsoup.connect(url).get
+            val tableClassements = classementPage.select("h3:has(> #Classement) ~ table.wikitable.gauche tbody tr")
+            for (e <- tableClassements) {
+              if(e.child(0).tagName() != "th") {
+                val position = e.child(0).text().toInt
+                val team = e.child(1).text()
+                val points = e.child(2).text().toInt
+                val played = e.child(3).text().toInt
+                val won = e.child(4).text().toInt
+                val drawn = e.child(5).text().toInt
+                val lost = e.child(6).text().toInt
+                val goalsFor = e.child(7).text().toInt
+                val goalsAgainst = e.child(8).text().toInt
+                val goalsDifference = e.child(9).text().toInt
+                logger.debug(league, team)
+                val ls = LeagueStanding(
+                  league, season, position, team, points, played, won, drawn, lost, goalsFor, goalsAgainst, goalsDifference
+                )
+                leagueStandings :+ ls
+              }
+            }
+            leagueStandings
           } catch {
             case _: Throwable =>
               // TODO Q3 En supposant que ce job tourne sur un cluster EMR, où seront affichés les logs d'erreurs ?
+              // N'ayant travailler que sur la Google Cloud Platform je ne maîtrise pas les environnements AWS
+              // Ils seront stockes /mnt/var/log/spark/*
               logger.warn(s"Can't parse season $season from $url")
               Seq.empty
           }
       }
       // TODO Q4 Comment partitionner les données en 2 avant l'écriture sur le bucket
-      .XXXXX
+      .splitAt(10)
       .write
       .mode(SaveMode.Overwrite)
       .parquet(bucket)
 
     // TODO Q5 Quel est l'avantage du format parquet par rapport aux autres formats ?
+    // Organiser en colonne
+    // Consomme moins d'espace
+    // Similaire à une base de données (sauvegarde en table avec colonne nommée)
+    // Récupération d'une colonne spécifique facile
 
     // TODO Q6 Quel est l'avantage de passer d'une séquence scala à un dataset spark ?
+    // Don't know
+    // J'assumerai que vu qu'une séquence est une sorte de liste indexés cela permet de faciliter la conversion
   }
 
   private def getLeagues: Seq[LeagueInput] = {
+    val inputStream = new FileReader("../resources/leagues.yaml")
     val mapper = new ObjectMapper(new YAMLFactory())
     // TODO Q7 Recuperer l'input stream du fichier leagues.yaml
     mapper.readValue(inputStream, classOf[Array[LeagueInput]]).toSeq
@@ -64,8 +101,8 @@ case class Q1_WikiDocumentsToParquetTask(bucket: String) extends Runnable {
 }
 
 // TODO Q8 Ajouter les annotations manquantes pour pouvoir mapper le fichier yaml à cette classe
-case class LeagueInput(name: String,
-                       url: String)
+case class LeagueInput(@JsonProperty("name") name: String,
+                       @JsonProperty("url") url: String)
 
 case class LeagueStanding(league: String,
                           season: Int,
--- src/main/scala/com/test/spark/wiki/extracts/Q2_ShowLeagueStatsTask.scala
+++ src/main/scala/com/test/spark/wiki/extracts/Q2_ShowLeagueStatsTask.scala
@@ -20,18 +20,38 @@ case class Q2_ShowLeagueStatsTask(bucket: String) extends Runnable {
     // TODO Q1
     println("Utiliser createTempView sur $standings et, en sql, afficher la moyenne de buts par saison et " +
       "par championnat")
+    standings.createOrReplaceTempView("leagueStands")
+    standings.sqlContext.sql("SELECT season, league, MEAN(goalsFor) FROM leagueStands GROUP BY season, league").show()
 
     // TODO Q2
     println("En Dataset, quelle est l'équipe la plus titrée de France ?")
+    standings.groupBy(standings.col("league")).agg(
+      min(mean(standings.col("position")))
+    ).show()
 
     // TODO Q3
     println("En Dataset, quelle est la moyenne de points des vainqueurs sur les 5 différents championnats ?")
+    standings
+      .filter("position = 1")
+      .groupBy(standings.col("league"), standings.col("team"))
+      .mean("points")
+      .show()
 
     // TODO Q5 Ecrire une udf spark "decade" qui retourne la décennie d'une saison sous la forme 199X ?
+    val decadeScala : Int => String = _.toString.dropRight(1) + "X"
+
+    val decade = udf(decadeScala)
+    session.udf.register("decade", (season : Int) => (season / 10).toString + "X") // Usable for sql usage
 
     // TODO Q4
     println("En Dataset, quelle est la moyenne de points d'écart entre le 1er et le 10ème de chaque championnats " +
       "par décennie")
-
+    standings
+      .filter("position = 1 or position = 10")
+      .withColumn("decade", decade('season))
+      .groupBy(standings.col("league"), standings.col("decade"))
+      .agg(
+        mean(max("position") - min("position"))
+      )
   }
 }
